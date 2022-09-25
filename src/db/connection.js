34ae230e12eb8661af213f36a31231dae742734c
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-09-25 19:02:56 -0400

    deployment

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,6 @@
-const environment = process.env.NODE_ENV || 'development' //was development
+const environment = 'development'
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
+console.log('hi', knex.client)
 
 module.exports = knex;
